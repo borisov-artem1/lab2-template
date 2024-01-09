46c96fc723cdabe4f240a91cf6ba62a8152e09e1
Ivan Tsvetkov <amunra23@MacBook-Pro-Ivan.local>
2024-01-09 11:43:46 +0300

    added models lib service

---
+++ src/library_service/models/library_book.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from utils.database import Base
+
+
+class LibraryBookModel(Base):
+  __tablename__ = "library_book"
+  __table_args__ = {'extend_existing': True}
+  
+  id              = Column(Integer, primary_key=True, index=True)
+  book_id         = Column(Integer, ForeignKey(BookModel.id))
+  library_id      = Column(Integer, ForeignKey(LibraryModel.id))
+  available_count = Column(Integer, nullable=False)
