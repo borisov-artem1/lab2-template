f03a001f9cfe1ed017f00a6d7deb636b90e83b6f
Ivan Tsvetkov <amunra23@MacBook-Pro-Ivan.local>
2024-01-14 21:27:52 +0300

    added reservation service

---
+++ src/reservation_service/app/schemas/reservation.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, constr, conint
+from datetime import datetime as dt
+from uuid import UUID
+
+from enums.status import ReservationStatus
+
+
+class ReservationBase(BaseModel):
+    username: constr(max_length=80)
+    book_uid: UUID
+    library_uid: UUID
+    status: ReservationStatus
+    start_date: dt
+    till_date: dt
+
+
+class ReservationFilter(BaseModel):
+    username: constr(max_length=80) | None = None
+    book_uid: UUID | None = None
+    library_uid: UUID | None = None
+    status: ReservationStatus | None = None
+    start_date: dt | None = None
+    till_date: dt | None = None
+    
+
+class ReservationUpdate(BaseModel):
+    username: constr(max_length=80) | None = None
+    book_uid: UUID | None = None
+    library_uid: UUID | None = None
+    status: ReservationStatus | None = None
+    start_date: dt | None = None
+    till_date: dt | None = None
+
+
+class ReservationCreate(ReservationBase):
+    pass
+
+
+class Reservation(ReservationBase):
+    id: int
+    reservation_uid: UUID
+    
